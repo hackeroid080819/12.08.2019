1f2d950525dfec788c5aad5a06c9f7c3ce87e3f9
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-08-12 15:20:19 +0300

    Add files via upload

---
+++ WebView/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.webview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView wv1 = findViewById(R.id.wv1);
+        wv1.setWebViewClient(new MyBrowser());
+
+        Button b1 = findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                wv1.getSettings().setLoadsImagesAutomatically(true);
+                wv1.getSettings().setJavaScriptEnabled(true);
+                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                wv1.loadUrl("https://www.w3schools.com/colors/colors_picker.asp");
+            }
+        });
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
